f14e0a0b45f79f3ad640d2ecf982758a80c8fa51
cjw <c3392302@naver.com>
2023-01-08 15:18:50 +0900

    css

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -180,6 +180,7 @@ const PostDetail = () => {
 
     },[]);
 
+    const img = postImg ? <img src={postImg } /> : <></>;
     
     
 
@@ -200,7 +201,7 @@ const PostDetail = () => {
                 <div className="date">{userId} | {postDate}</div>
                 <figure className="postImage">
                     <div className="placeImg">
-                        <img src={postImg} />
+                        {img}
                     </div>
                 </figure>
                 <section className="content">
